ba43b73459e7c89ec4c39bff0dae20d6188fa0bc
Franz Rosete <frosete@myseneca.ca>
2026-03-25 05:46:11 -0400

    added two functions (show_info and validate_points)also added comments for main block logic and for the functions

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,35 @@
 #!/usr/bin/env python3
 
 import argparse
+import random
 
-'''
-Enter Your Code Here
-'''
+#Shows game information to user
+def show_info():
+
+
+#Shows current points between players
+def validate_points(points):
+
+
+
+def number_to_choose(number):
+ 
+
+
+def get_computer_choice():
+    
+if __name__ == '__main__':
+    '''
+     Show user game info
+     give user choice between gameinfo or starting game
+     use a while loop for error checking until right input is given
+     game logic here
+     while loop will be used to repeat game logic until a player reaches winning points
+     give user choices between rock,paper or scissors while loop will be used for error checking until right input is given
+     determine result of current round
+     if draw one point randomly append to a player
+     if win append two points
+     if lose remove one point
+     show round result to user
+     
+    '''
